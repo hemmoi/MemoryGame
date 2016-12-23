21e9b4d1ab390ea4f29ca916a0da46d9635a7b0b
Kimmo Halonen <Kimmo Halonen>
2016-12-23 14:32:23 +0200

    Christmas special

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -10,11 +10,16 @@ var Card = React.createClass({
 
   render: function () {
     var images = [
-        "http://pngimg.com/upload/dog_PNG2422.png",
-        "http://pngimg.com/upload/cat_PNG1631.png",
-        "http://pngimg.com/upload/parrot_PNG723.png",
-        "http://pngimg.com/upload/small/pig_PNG2210.png",
-        "http://pngimg.com/upload/cow_PNG2132.png",
+//        "http://pngimg.com/upload/dog_PNG2422.png",
+        "http://wallpapercave.com/wp/n9dSK6w.jpg",
+//        "http://pngimg.com/upload/cat_PNG1631.png",
+        "https://s-media-cache-ak0.pinimg.com/236x/8d/cf/1c/8dcf1cb676793bd0e867fd1fb003e0c7.jpg",
+//        "http://pngimg.com/upload/parrot_PNG723.png",
+        "http://ih1.redbubble.net/image.59719460.9792/flat,800x800,075,t.jpg",
+//        "http://pngimg.com/upload/small/pig_PNG2210.png",
+        "http://vignette1.wikia.nocookie.net/pirates/images/1/1a/OSTBarbossaPegLeg.jpg/revision/latest?cb=20110727092535",
+//        "http://pngimg.com/upload/cow_PNG2132.png",
+        "http://ep.yimg.com/ay/randalls/riga-black-balsam-liqueur-1.gif",
         "http://www.clipartbest.com/cliparts/ncX/Lyo/ncXLyoBcB.png",
         "https://s-media-cache-ak0.pinimg.com/564x/5c/d1/2d/5cd12dede90464b6d0200ab43dd46e41.jpg"
     ];
